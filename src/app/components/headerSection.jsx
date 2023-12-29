d97cc173e1afebf5cff83043932acf05fd5720ea
ersi <ersildacako14@gmail.com>
2023-12-28 23:56:06 -0500

    Starting header section

---
+++ src/app/components/headerSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const header = () => {
+  return (
+    <section>
+        <h1 className="text-white">Hello I'm Ersi</h1>
+    </section>
+  )
+}
+
+export default header
